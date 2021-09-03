31be767ecd55f6b9d69d3de66f2ebade74ec0ba3
HEKUCHAN <Heitorhirose@gmail.com>
2021-09-03 18:30:33 +0900

    開発始めれるように設定完了

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# APIsのインポート
+from rest_framework import viewsets, status, authentication
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+# Serializerのインポート
+
+# Modelsのインポート
