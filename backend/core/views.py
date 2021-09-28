5fe5b6d849818348ffd1995aa1d6f70129fcb779
HEKUCHAN <Heitorhirose@gmail.com>
2021-09-28 21:36:09 +0900

    Djangoの環境構築をし直す

--- backend/core/views.py
+++
@@ -1,9 +0,0 @@
-# APIsのインポート
-from rest_framework import viewsets, status, authentication
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-# Serializerのインポート
-
-# Modelsのインポート
