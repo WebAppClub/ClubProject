4747cd6e972bc5eefb18e30396ffc2193877a039
HEKUCHAN <Heitorhirose@gmail.com>
2021-12-13 23:49:02 +0900

    add new test lib

--- api/config/settings.py
+++ api/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_cleanup.apps.CleanupConfig",
+    "django_nose",
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
